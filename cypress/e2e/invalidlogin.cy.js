f25d11670c10a116da488fe54fdcc7466a5241c2
ddyrnes <dev.dyrnes@gmail.com>
2024-04-24 16:45:01 +0200

    Added invalidlogin cypress test

---
+++ cypress/e2e/invalidlogin.cy.js
@@ -0,0 +1,26 @@
+describe('Invalid login test', () => {
+    beforeEach(() => {
+        cy.visit('index.html');
+        cy.wait(2000); // Wait for elements to be visible before interacting with them
+    });
+
+    it('should display an error message for invalid credentials', () => {
+        // Click the login button to open the login modal
+        cy.get("button[data-bs-target='#loginModal']:visible").first().click();
+
+        // Fill in the login form with invalid credentials
+        cy.get("#loginModal input[type='email']").type('invalid@stud.noroff.no');
+        cy.get("#loginModal input[type='password']").type('invalid');
+
+        // Click the submit button within the login modal
+        cy.get("#loginModal button[type='submit']").click();
+
+        // Check if an error message is displayed
+        cy.on('window:alert', (text) => {
+            expect(text).to.include('Either your username was not found or your password is incorrect');
+        });
+
+        // Check if token is not stored in localStorage after login attempt
+        cy.window().its('localStorage').should('not.have.property', 'token');
+    });
+});
