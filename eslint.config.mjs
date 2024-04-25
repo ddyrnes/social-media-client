03723406e9166c07b4ef459e175eb62ecf69b3b0
ddyrnes <dev.dyrnes@gmail.com>
2024-04-25 16:33:46 +0200

    Reinstalled eslint&babel, setup new config files. Linting now works properly on the whole project

--- eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
-
-export default [
-  {ignores: ["cypress.config.js"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended, 
-];
